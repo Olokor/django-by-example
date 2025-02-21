7c8ee927e1e3ca0c03351f4e2e6a3342546aa634
Wisdom Olokor <olokorwisdom15@gmail.com>
2025-02-21 15:13:24 +0000

    share post via email

--- blog-application/mysite/blog/models.py
+++ blog-application/mysite/blog/models.py
@@ -1,9 +1,9 @@
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
-
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
@@ -17,18 +17,24 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     publish = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     objects = models.Manager()
-    publised = PublishedManager()
+    published = PublishedManager()
     class Meta:
         ordering = ['-publish']
         indexes = [models.Index(fields=['-publish'])]
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        # return reverse("blog:post_detail", kwargs={"slug": self.slug})
+        return reverse("blog:post_detail", args=[self.publish.year
+                                                 , self.publish.month,
+                                                 self.publish.day, self.slug])
     
--- blog-application/mysite/blog/views.py
+++ blog-application/mysite/blog/views.py
@@ -1,19 +1,74 @@
+from tkinter.font import names
+
 from django.http import Http404
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.template.context_processors import request
+
+from .form import EmailPostForm
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
+
 # Create your views here.
 
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{form_data['name']} ({form_data['email']}) recommends you read {post.title}"
+            )
+
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{form_data['name']}'s comment: {form_data['comment']}"
+            )
+
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,  # Use a valid email
+                recipient_list=[form_data["recipient"]],
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
+class PostListView(ListView):
+    model = Post
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+    context_object_name = "posts"
 def post_list(request):
-    posts = Post.publised.all()
+    all_posts = Post.published.all()
+    paginator = Paginator(all_posts, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(
         request, "blog/post/list.html", {"posts":posts}
     )
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post ):
     # try:
     #     post = Post.publised.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("No Post Found")
 
-    post = get_list_or_404(Post, id=id, status=Post.Status.Published)
+    post = get_object_or_404(Post,  status=Post.Status.PUBLISHED,
+                           slug=post, publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {"post":post})
